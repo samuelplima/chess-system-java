095b1224be837db946d85af699dc88c6c6d83ac2
smk-1991 <samuel.lima1991@gmail.com>
2021-09-28 15:12:10 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,7 +28,8 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			} catch (ChessException e) {
+			}
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
